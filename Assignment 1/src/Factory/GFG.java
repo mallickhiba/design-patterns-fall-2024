6f4e238ce1277d5707dd736658778f1dfee99b22
mallickhiba <mallickhiba@gmail.com>
2024-10-11 01:50:33 +0500

    refactoring project struture (everything in src folder)

--- Assignment 1/src/Factory/GFG.java
+++ Assignment 1/src/Factory/GFG.java
@@ -3,5 +3,9 @@ public class GFG {
         Client client = new Client(VehicleType.VT_TwoWheeler);
         Vehicle vehicle = client.getVehicle();
         vehicle.printVehicle();
+
+        Client client2 = new Client(VehicleType.VT_FourWheeler);
+        Vehicle vehicle2 = client2.getVehicle();
+        vehicle2.printVehicle();
     }
 }
---
+++ Assignment 1/src/Singleton/Singleton.java
@@ -0,0 +1,22 @@
+public class Singleton {
+    private static volatile Singleton instance;
+
+    private Singleton() {
+    }
+    public static Singleton getInstance() {
+        if (instance == null) {
+            Class var0 = Singleton.class;
+            synchronized (Singleton.class) {
+                if (instance == null) {
+                    instance = new Singleton();
+                }
+            }
+        }
+
+        return instance;
+    }
+    public static void main(String[] args) {
+        Singleton singletonInstance = Singleton.getInstance();
+        System.out.println("Singleton instance: " + singletonInstance);
+    }
+}
